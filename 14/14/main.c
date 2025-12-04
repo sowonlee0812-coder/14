efd293a367602ad819fdff5e325c61b59dff6d9d
sowonlee0812-coder <sowon.lee0812@gmail.com>
2025-12-04 15:18:11 +0900

    14-1

---
+++ 14/14/main.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+    char *pc = NULL;
+    int i = 0;
+    
+    pc = (char*)malloc(100*sizeof(char));
+    if(pc == NULL){
+        printf("ERROR\n");
+        exit(1);
+    }
+    
+    for (i=0;i<26;i++)
+        pc[i] = 'a' + i;
+    pc[i] = 0;
+    
+    printf("%s\n", pc);
+    
+    free(pc);
+    
+    system("PAUSE");
+    return 0;
+}
